95a466e706ea02e0ad0c99585220e04a0ca27290
Mahammad Bakr <49376835+mahammadbakr@users.noreply.github.com>
2023-08-29 14:55:36 +0300

    first commit

---
+++ lib/components/app_bar.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+        height: 90,
+        width: MediaQuery.of(context).size.width,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 26,
+              backgroundImage: Image.network(
+                'https://t3.ftcdn.net/jpg/02/99/04/20/360_F_299042079_vGBD7wIlSeNl7vOevWHiL93G4koMM967.jpg',
+                fit: BoxFit.cover,
+              ).image,
+            ),
+            const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text('Delivery Address'),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.pin_drop_sharp,
+                      color: Colors.red,
+                    ),
+                    SizedBox(width: 6),
+                    Text('Raparin, Shexalla',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        )),
+                    SizedBox(width: 6),
+                    Icon(Icons.arrow_drop_down),
+                  ],
+                ),
+              ],
+            ),
+            Container(
+              height: 50,
+              width: 50,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(40),
+                border: Border.all(color: Colors.black26)
+              ),
+              child: const Icon(Icons.notifications_on_outlined),
+            )
+          ],
+        ),
+      );
+  }
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/screens/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void didChangeDependencies() {
+    Future.delayed(
+      const Duration(seconds: 1),
+      () {
+       Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const HomeScreen(),
+          ),
+       );
+      },
+    );
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          'assets/logo.png',
+          width: MediaQuery.of(context).size.width / 2,
+        ),
+      ),
+    );
+  }
+}
